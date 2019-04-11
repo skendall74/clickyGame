dfa8055ca3c132c47219fdd4611b1495086aacf8
skendall <shannonckendall@gmail.com>
2019-04-11 13:54:43 -0400

    Final Push for Deploy

--- src/App.js
+++ src/App.js
@@ -19,7 +19,8 @@ class App extends Component {
   state = {
     count: 0,
     score: 0,
-    selectedCards: [],
+    highScore: 0,
+    selectedCards: []
   };
 
   selectedImage = id => {
@@ -28,6 +29,39 @@ class App extends Component {
     let highScore = this.state.highScore;
     // Set this.state.cards equal to the new cards array
     // if the clicked image has an id of the indexed cards
+    switch (selectedCards) {
+      case selectedCards.indexOf(id) === -1:
+        selectedCards.push(id);
+        // run the score function
+        this.handleIncrement();
+        // run the reshuffle function after each click
+        this.cardShuffle();
+        break;
+
+      case this.state.score === 12:
+        alert("You WON!");
+        this.setState({
+          score: 0,
+          selectedCards: []
+        });
+        break;
+
+      case this.setState:
+        this.setState({
+          score: 0,
+          selectedCards: []
+        });
+        alert("YOU LOST! You clicked an image twice!");
+        break;
+
+      case score > highScore:
+        this.setState({
+          highScore: score
+        });
+        break;
+      default:
+    }
+   
     if (selectedCards.indexOf(id) === -1) {
       // push that id into that id into the array to be stored
       selectedCards.push(id);
@@ -60,7 +94,7 @@ class App extends Component {
   handleIncrement = () => {
     // We always use the setState method to update a component's state
     this.setState({ score: this.state.score + 1 }, () => {
-      console.log(this.state.score)
+      console.log(this.state.score);
     });
   };
 
@@ -83,6 +117,7 @@ class App extends Component {
         <Title>Clicky Game</Title>
         <div className="row">
           <div className="col s12">
+            <h1>High Score: {this.state.highScore}</h1>
             <h1>Score: {this.state.score}</h1>
           </div>
         </div>
